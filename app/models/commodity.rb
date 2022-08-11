f84cd06d3866c124145b908f32624414e06db9eb
akitomato <tobewxjp24x@gmail.com>
2022-08-11 13:57:34 +0900

    商品出品機能修正１

--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -6,25 +6,24 @@ class Commodity < ApplicationRecord
   belongs_to :condition
   belongs_to :delivery_charge
   has_one_attached :image
-  #belongs_to :user
-  #has_one :buy
+  # belongs_to :user
+  # has_one :buy
 
   with_options presence: { message: 'は必須項目です' } do
     validates :name
     validates :detail
     validates :image
   end
-  
-  with_options presence: true, numericality: { other_than: 1 ,message: '項目の選択必須です'} do
+  with_options presence: true, numericality: { other_than: 1, message: '項目の選択必須です' } do
     validates :category_id
     validates :prefecture_id
     validates :send_day_id
     validates :condition_id
     validates :delivery_charge_id
   end
-  
-  with_options presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 999999999, message: '価格は指定範囲内で入力必須です。(半角数字でご入力ください)' } do 
+  with_options presence: true, numericality: { 
+    only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 999999999,
+    message: '価格は指定範囲内で入力必須です。(半角数字でご入力ください)' } do
     validates :price
   end
-  
 end
