e8912abb644c4056cb47fc256fda7c041c12791e
akitomato <tobewxjp24x@gmail.com>
2022-08-12 16:53:35 +0900

    商品詳細表示機能実装１

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "commodities#index"
-  resources :commodities, only: [:new, :create] do
+  resources :commodities, only: [:new, :create, :show] do
   end
 end
